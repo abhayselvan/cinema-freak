7600bc04be369e2f88b2a23af3adfbb313b3591d
Arthi-K <81531516+Arthi-K@users.noreply.github.com>
2021-11-21 19:44:55 -0800

    Authetication completed

--- app/src/main/java/activity/Login.java
+++ app/src/main/java/activity/Login.java
@@ -1,14 +1,110 @@
 package activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.cinemaFreak.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private TextView register,forgotPassword;
+    private EditText editTextUsername, editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView)findViewById(R.id.registerHere);
+        register.setOnClickListener(this);
+
+        forgotPassword = (TextView)findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.login);
+        signIn.setOnClickListener(this);
+
+        editTextUsername = (EditText) findViewById(R.id.userName);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.registerHere:
+                startActivity(new Intent(this,Register.class));
+                break;
+
+            case R.id.login:
+                userLogin();
+                break;
+
+            case R.id.forgotPassword:
+                startActivity(new Intent(this,ForgotPassword.class));
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            editTextUsername.setError("Username/Email is required!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextUsername.setError("Please provide valid email");
+            editTextUsername.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            editTextPassword.setError("Minimum password length is 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //redirect to user profile
+                    startActivity(new Intent(Login.this,MovieSelection.class));
+                }
+                else
+                {
+                    Toast.makeText(Login.this,"Failed to login! Please check your credentials!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
