92923c647803eb37c65107dab6f7222188a38db9
Janan Gandhi <janangandhi94@gmail.com>
2021-11-23 21:04:29 -0800

    Like | Dislike | watch later fix

--- app/src/main/java/activity/MovieDescription.java
+++ app/src/main/java/activity/MovieDescription.java
@@ -10,12 +10,12 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -28,6 +28,8 @@ import com.cinemaFreak.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -41,6 +43,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import data.MovieItem;
 import database.DatabaseInstance;
@@ -53,18 +56,19 @@ public class MovieDescription extends YouTubeBaseActivity {
 
     private static final int PERMISSIONS_COARSE_LOCATION = 1;
     public static final String TAG = "DescriptionActivity";
-
-    String title, description, countryCode, countryName;
-    String providerUrl, posterUrl, trailerLink;
-    TextView movieTitle, movieDescription, streamHeading;
-    LinearLayout linearLayout;
-    ImageView poster, like;
-    JSONObject providerDetails;
-    InputStream inputStream;
-    Bitmap bitmap;
+    private Handler handler;
+    private String title, description, countryCode, countryName;
+    private String providerUrl, posterUrl, trailerLink;
+    private TextView movieTitle, movieDescription, streamHeading;
+    private LinearLayout linearLayout;
+    private ImageView poster, like, bookmark, dislike;
+    private JSONObject providerDetails;
+    private InputStream inputStream;
+    private Bitmap bitmap;
+    private boolean isMovieLiked, isMovieDisliked, isMovieBookmarked;
 
     MovieItem movie;
-    User user;
+    User activeUser;
 
     YouTubePlayerView mYouTubePlayerView;
     YouTubePlayer.OnInitializedListener mOnInitializedListener;
@@ -80,13 +84,8 @@ public class MovieDescription extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_description);
 
-        Log.i(TAG, "OnCreate");
-
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-
-        movie = (MovieItem) bundle.get("movieId");
-        user = ((CinemaFreakApplication)getApplication()).getActiveSessionUser();
+        movie = (MovieItem) getIntent().getExtras().get("movieId");
+        activeUser = ((CinemaFreakApplication)getApplication()).getActiveSessionUser();
 
         queue = Volley.newRequestQueue(this);
         movieTitle = findViewById(R.id.movie_title);
@@ -96,20 +95,26 @@ public class MovieDescription extends YouTubeBaseActivity {
         poster = findViewById(R.id.poster);
         linearLayout = findViewById(R.id.providers);
         like = findViewById(R.id.like);
+        bookmark = findViewById(R.id.bookmark);
+        dislike = findViewById(R.id.dislike);
         poster.setImageBitmap(null);
+        isMovieLiked = activeUser.getLikedMovies().stream().map(MovieItem::getId).collect(Collectors.toList()).contains(movie.getId());
+        isMovieDisliked = activeUser.getDislikedMovies().stream().map(MovieItem::getId).collect(Collectors.toList()).contains(movie.getId());
+        isMovieBookmarked = activeUser.getBookmarkedMovies().stream().map(MovieItem::getId).collect(Collectors.toList()).contains(movie.getId());
 
+        updateIcons();
+        bookmark.setOnClickListener(view -> onBookmarkPress());
+        like.setOnClickListener(view -> onLikePressed());
+        dislike.setOnClickListener(view -> onDislikePressed());
+
+        handler = new Handler();
         providerUrl = "https://" + Constants.TMDB_HOST_URL + Constants.MOVIE_PATH + "/" + movie.getId() + Constants.WATCH_PROVIDERS + "?" + Constants.API_KEY_PARAM + "=" + Constants.API_KEY;
         posterUrl = Constants.TMDB_POSTER_PATH;
 
-        if (user.getLikedMovies().contains(movie))
-            like.setImageResource(R.drawable.ic_baseline_thumb_up_30_red);
-
-        Log.i(TAG, user.getLikedMovies().toString());
         getDetails();
         getTrailer();
 
-        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, providerUrl, null,
-                response -> {
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, providerUrl, null, response -> {
                     Log.i(TAG, "Movie API received");
                     providerDetails = response;
                     getLocation();
@@ -121,14 +126,54 @@ public class MovieDescription extends YouTubeBaseActivity {
         locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
         queue.add(jsonObjectRequest);
+    }
 
-        like.setOnClickListener(view -> {
-            if (!user.getLikedMovies().contains(movie)) {
-                user.addLikedMovieItem(movie);
-                like.setImageResource(R.drawable.ic_baseline_thumb_up_30_red);
-                DatabaseInstance.DATABASE.getReference().child("Users").child(user.getId()).child("likedMovies").setValue(user.getLikedMovies());
-            }
-        });
+    private void onLikePressed(){
+        if (isMovieLiked) {
+            removeLike();
+        } else {
+            if(isMovieDisliked)
+                removeDislike();
+            activeUser.addLikedMovieItem(movie);
+            like.setImageResource(R.drawable.ic_baseline_thumb_up_30_red);
+            isMovieLiked=true;
+        }
+    }
+
+    private void onDislikePressed(){
+        if (isMovieDisliked) {
+            removeDislike();
+            //DatabaseInstance.DATABASE.getReference().child("Users").child(activeUser.getId()).child("likedMovies").setValue(activeUser.getLikedMovies());
+        } else {
+            if(isMovieLiked)
+                removeLike();
+            activeUser.addDislikedMovieItem(movie);
+            dislike.setImageResource(R.drawable.ic_baseline_thumb_down_30_red);
+            isMovieDisliked=true;
+        }
+    }
+
+    private void removeLike(){
+        activeUser.removeLikedMovieItem(movie.getId());
+        like.setImageResource(R.drawable.ic_baseline_thumb_up_30);
+        isMovieLiked=false;
+    }
+
+    private void removeDislike(){
+        activeUser.removeDislikedMovie(movie.getId());
+        dislike.setImageResource(R.drawable.ic_baseline_thumb_down_30);
+        isMovieDisliked=false;
+    }
+
+    private void onBookmarkPress(){
+        if (isMovieBookmarked) {
+            activeUser.removeBookmarkedMovies(movie.getId());
+            bookmark.setImageResource(R.drawable.ic_bookmark);
+        } else {
+            activeUser.addBookmarkedMovies(movie);
+            bookmark.setImageResource(R.drawable.ic_bookmark_fill);
+        }
+        isMovieBookmarked = !isMovieBookmarked;
     }
 
     private void getDetails() {
@@ -291,6 +336,30 @@ public class MovieDescription extends YouTubeBaseActivity {
             }
         }
 
+    }
+
+    private void updateIcons(){
+        if (isMovieLiked)
+            like.setImageResource(R.drawable.ic_baseline_thumb_up_30_red);
+
+        if (isMovieDisliked)
+            dislike.setImageResource(R.drawable.ic_baseline_thumb_down_30_red);
+
+        if (isMovieBookmarked)
+            bookmark.setImageResource(R.drawable.ic_bookmark_fill);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ((CinemaFreakApplication)getApplication()).setActiveSessionUser(activeUser);
+        handler.post(() -> DatabaseInstance.DATABASE.getReference().child("Users").child(activeUser.getId()).setValue(activeUser).addOnCompleteListener(task -> {
+            if(task.isComplete()){
+                Log.i(TAG, "Updated user in db: "+activeUser);
+            } else {
+                Log.i(TAG, "Error in updating db: "+task.getException());
+            }
+        }));
     }
 
     @Override
