90ed4840211d70ba82491e1d49148136006d1498
AshitaShetty <shettyashita93@gmail.com>
2021-11-30 17:25:54 -0800

    solved scrolling issue

--- app/src/main/java/activity/MovieRecommendation.java
+++ app/src/main/java/activity/MovieRecommendation.java
@@ -3,11 +3,6 @@ package activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
