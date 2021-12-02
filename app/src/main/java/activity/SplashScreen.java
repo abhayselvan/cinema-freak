76e5f7a2583f94763e57c212206af765a8b52bab
nkamath96 <nkamath@scu.edu>
2021-12-02 11:58:45 -0800

    Updated with Logo Animation

--- app/src/main/java/activity/SplashScreen.java
+++ app/src/main/java/activity/SplashScreen.java
@@ -2,30 +2,44 @@ package activity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cinemaFreak.R;
 
 public class SplashScreen extends AppCompatActivity {
+    private TextView cinemaTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        cinemaTextView = findViewById(R.id.cinema_textView);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         int secondsDelayed = 1;
+        Animation cinema_text_anim = AnimationUtils.loadAnimation(this, R.anim.cinema_text_view);
+        cinemaTextView.startAnimation(cinema_text_anim);
+        VideoView videoview = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash_final);
+        videoview.setVideoURI(uri);
+        videoview.start();
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 startActivity(new Intent(SplashScreen.this, Login.class));
                 finish();
             }
-        }, secondsDelayed * 2000);
+        }, secondsDelayed * 3000);
     }
 }
