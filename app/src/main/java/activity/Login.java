6021b2a017231fa9ed09a23d70d1ecf322bd7e19
AshitaShetty <shettyashita93@gmail.com>
2021-12-01 22:26:32 -0800

    Login and register page

--- app/src/main/java/activity/Login.java
+++ app/src/main/java/activity/Login.java
@@ -1,8 +1,5 @@
 package activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,24 +7,38 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.cinemaFreak.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import data.Config;
+import data.FileUtil;
+import data.MovieItem;
 import util.Constants;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "CinemaFreak-Login";
+    private static final String CONFIG_PATH = "config.json";
     private TextView register,forgotPassword;
     private EditText editTextUsername, editTextPassword;
     private Button signIn;
-
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
     private FirebaseAuth mAuth;
+    ImageView imageView;
+    Config config;
+    private final List<MovieItem> allMovies = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +59,20 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         mAuth = FirebaseAuth.getInstance();
 
+        // Load config file.
+        try {
+            config = FileUtil.loadConfig(getAssets(), CONFIG_PATH);
+        } catch (IOException ex) {
+            Log.e(TAG, String.format("Error occurs when loading config %s: %s.", CONFIG_PATH, ex));
+        }
+        // Load movies list.
+        try {
+            allMovies.clear();
+            allMovies.addAll(FileUtil.loadMovieList(getAssets(), config.movieSelectionList));
+        } catch (IOException ex) {
+            Log.e(TAG, String.format("Error occurs when loading movies %s: %s.", config.movieSelectionList, ex));
+        }
+        imageView = findViewById(R.id.imageview);
     }
 
     @Override
